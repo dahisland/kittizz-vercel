44bbf189cf81b8da75b8e38164384768116a7a2f
Myriam Mornet <dahisland@gmail.com>
2023-07-20 01:00:48 +0200

    Modify Hashrouter to Browserrouter in client and create new build

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import About from "./pages/about/About";
 import Kitty from "./pages/kitty/Kitty";
 
 function App() {
   return (
-    <HashRouter basename="/">
+    <BrowserRouter basename="/">
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/about" element={<About />} />
         <Route path="/kitty/:kittyID" element={<Kitty />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
